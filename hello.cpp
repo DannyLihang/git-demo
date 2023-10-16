197551e9c46087e17cdcacedb2dd8a80fddff388
DannyLihang <2397936316@qq.com>
2023-10-16 14:54:17 +0800

    first commit cpp

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+int main()
+{
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+        cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	cout >> "hello world !" >> endl;
+	return 0;
+}
